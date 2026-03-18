875a636c2a2ec6234ea4d09714fd1109a400a908
nino <pilopilosimon1990@gmail.com>
2026-03-18 19:54:13 +0100

    Update and rename twrp_m16xm.mk to twrp_x15m.mk

---
+++ twrp_x15m.mk
@@ -0,0 +1,13 @@
+# Release name
+PRODUCT_RELEASE_NAME := F15 5G
+
+# Inherit from m16xm device
+$(call inherit-product, device/samsung/x15m/device.mk)
+
+PRODUCT_DEVICE := m16xm
+PRODUCT_NAME := twrp_x15m
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Samsung f15 5g
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/x15m/recovery/root,recovery/root)
