41c03f55bd669b02d19c47077a28925273d476d6
nino <pilopilosimon1990@gmail.com>
2026-03-18 23:09:57 +0100

    Update twrp_x15m.mk

--- twrp_x15m.mk
+++ twrp_x15m.mk
@@ -11,3 +11,8 @@ PRODUCT_MODEL := Samsung f15 5g
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/x15m/recovery/root,recovery/root)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="m15xnndxx-user 13 TP1A.220624.014 E156BXXS5AYA1 release-keys"
+
+BUILD_FINGERPRINT := samsung/m15xnndxx/m15x:13/TP1A.220624.014/E156BXXS5AYA1:user/release-keys
